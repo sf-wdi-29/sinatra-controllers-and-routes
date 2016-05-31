a97e67fbac7c0b303d07b3317269cb12539e0ddf
Ben Hulan <benhulan@gmail.com>
2016-05-31 16:41:55 -0700

    adds in-class code

--- solution-code/restful_controller_app/app.rb
+++ solution-code/restful_controller_app/app.rb
@@ -1,51 +1,50 @@
 class RestfulControllerApp < Sinatra::Base
 
-    # INDEX - Like index.html, a list of books
-    # GET "/books" - Gets all the books
+    get "/" do
+       erb "Sanity Check"  
+    end
+
+    # INDEX
+    # GETs all the books
     get "/books" do
-        # some code will go here    
+        @books = ["First book", "Second book"]
+        erb :books
     end
 
-    # NEW - a page that gives us a form to make a new book
-    # GET "/books/new" - Give us a form to fill out the details of a new book
+    # NEW -- we need a route to serve the form to create a new book
     get "/books/new" do
-        # some code will go here    
+        erb :'new'
     end
 
-    # CREATE - where the NEW form POSTs. We want to add our new book to all the books
-    # POST "/books" - Create a new book and add it to our list
+    # SHOW
+    # GET /books/3 gets one book with id = 3 to show the details
+    get "/books/:id" do    # would really be "/books/:id"
+        erb "Shows Book " + params[:id]
+    end
+
+
+    # CREATE
+    # POST a new book to all the books
     post "/books" do
-        # some code will go here 
+        # add code here
+        erb "When we submit a new book from the new book form, we post to /books"
     end
 
-    # SHOW - show the details about just one book
-    # GET "/books/3" - Just get the information associated with book 3
-    get "/books/:id" do
-        # params[:id]   # => would return 3 in our example
-        # some more code will go here 
-    end
-
-    # EDIT - like NEW, this just gives us a form that will PUT/PATCH our changes
-    # GET "/books/3" - Just get the information associated with book 3
+    # EDIT
     get "/books/:id/edit" do
-        # some code will go here 
+        @params = params[:id]
+        erb :'edit'
     end
 
-    # UPDATE - like CREATE, this does the actual updating
-    # PUT "/books/3" - Updates a specific book (book id = 3)
-    put "/books/:id" do
-        # some code will go here 
+    # UPDATE
+    put "/books/:id" do 
+        erb "Now I have just edited book 3"
     end
 
-    # UPDATE - believe it or not, PUT & PATCH are often the same code, so many developers skip PATCH and just use PUT
-    # PATCH "/books/3" - Partially updates a specific book (book id = 3)
-    patch "/books/:id" do
-        # some code will go here 
-    end
-
-    # DESTROY - totally nukes a book from the database
-    # DELETE "/books/3" - Deletes a specific book (book id = 3)
+    # DESTROY
     delete "/books/:id" do
-        # some code will go here 
+        erb "Book "  + params[:id] +  " is now dead"   # this should render /books
     end
+
+
 end
